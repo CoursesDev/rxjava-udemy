d3c83fdbaa19dad9cb5cc9734995d6367eed4d27
AngeOro <angelica-om@outlook.com>
2022-02-11 21:28:23 -0500

    Metodos de referencia

---
+++ javarx/src/main/java/co/com/poli/lambda/Referencia.java
@@ -0,0 +1,20 @@
+package co.com.poli.lambda;
+
+import java.util.function.BiFunction;
+
+public class Referencia {
+    public static void main(String[] args) {
+
+        BiFunction<String, String, String> biFunction = A_Class :: staticMethod;
+
+        System.out.println(biFunction.apply("Basics", "Strong"));
+
+    }
+}
+
+class A_Class{
+
+    static String staticMethod(String a, String b) {
+        return a+b;
+    }
+}
